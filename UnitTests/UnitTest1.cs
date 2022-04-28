3bfe2f082f1ad1df5f306b945bdc62eac9a4f005
Jez Horton <jez_98@hotmail.co.uk>
2022-04-28 10:22:39 +0100

    Added test project and test outline

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestClass]
+        public class FileTests
+        {
+
+            [TestMethod]
+            public void FileStreamFailedName()
+            {
+            }
+            [TestMethod]
+            public void FileStreamFailedPath()
+            {
+            }
+        }
+        [TestClass]
+        public class FilterTests
+        {
+            [TestMethod]
+            public void FilterVowel()
+            {
+                //Open File test with valid input
+            }
+            [TestMethod]
+            public void FilterConsanant()
+            {
+                //Open File test with invalid input
+            }
+            [TestMethod]
+            public void FilterFromMiddle()
+            {
+                //Open File test with valid input
+            }
+            [TestMethod]
+            public void FilterWordsLessThanThree()
+            {
+            }
+            [TestMethod]
+            public void FilterWordsMoreThanThree()
+            {
+            }
+            [TestMethod]
+            public void FilterWordsContainingT()
+            {
+            }
+            [TestMethod]
+            public void FilterWordsNotContainingT()
+            {
+            }
+        }
+    }
+}
